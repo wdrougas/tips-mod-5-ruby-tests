f856fb222e0e18f4ed9041342519f5b1dbab8435
Will Drougas <wdrougas@gmail.com>
2020-02-03 08:52:54 -0500

    notes

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -2,24 +2,31 @@ require 'active_support'
 require 'active_support/core_ext'
 
 class RecurringMoment
+  #every instance of a recurring moment has a start, an interval, and a period
+  #the instance variable of start would be a date i believe
+  #the instance variable of interval would be an amount of time, such as 1 day, 1 week, or 1 month
+  #the period would be how frequently this moment reoccurs. Daily, monthly, or weekly 
+  #lunch_with_mom = RecurringMoment.new(start: jan1, interval: 1 , period: 'weekly')
+  #lunch_with_mom.match(start: jan1.advance(weeks: 1)).to eq true
   def initialize(start:, interval:, period:)
     @start = start
     @interval = interval
     @period = period
   end
 
+  #instance method for an instance of RecurringMoment. Takes in one argument of a date and based on the date determines if it is a match
   def match(date)
     current = @start
 
     while current < date
       if current == date
-        return true
-      end
-
-      if @period == 'monthly'
-        current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
+      elsif @period == 'monthly' 
         current = current.advance(months: @interval)
+      elsif @period == 'weekly'
+        current = current.advance(weeks: @interval)
+        #here we need the interval to be a multiple of the start value. If we have a 2 day recurring moment, we need to return false for an interval of a week or an odd number of days
+      #elsif @period == 'daily' && current % @interval == 0
+      #current.strftime('%D') = current.advance(days: @interval)
       elsif @period == 'daily'
         current = current.advance(days: @interval)
       end
